d4d5861010c7fc51c945fdac6834594c819fdbf5
byun970 <byun970@gmail.com>
2025-09-23 17:51:04 +0900

    메인페이지 스타일 및 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,9 @@
-import "./App.css";
-import { Route, Routes, Navigate, Link } from "react-router-dom";
+import Router from "./components/Router";
 
 function App() {
   return (
     <>
-      <div>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/posts'>Post Link</Link>
-          </li>
-          <li>
-            <Link to='/posts/:id'>Post Detail</Link>
-          </li>
-          <li>
-            <Link to='/posts/new'>Post New</Link>
-          </li>
-          <li>
-            <Link to='/posts/edit/:id'>Post Edit</Link>
-          </li>
-          <li>
-            <Link to='/profile'>Profile</Link>
-          </li>
-        </ul>
-      </div>
-      <Routes>
-        <Route path='/' element={<h1>HomePage</h1>} />
-        <Route path='/posts' element={<h1>Post List Page</h1>} />
-        <Route path='/posts/:id' element={<h1>Post Detail Page</h1>} />
-        <Route path='/posts/new' element={<h1>Post New Page</h1>} />
-        <Route path='/posts/edit/:id' element={<h1>Post New Page</h1>} />
-        <Route path='/profile' element={<h1>Post New Page</h1>} />
-        <Route path='*' element={<Navigate replace to='/' />} />
-      </Routes>
+      <Router />
     </>
   );
 }
